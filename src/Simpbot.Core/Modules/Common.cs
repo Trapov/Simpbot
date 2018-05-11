43f0e58430e53af88634a5e9f59b63045e3af613
Afonin Dmitry <dmitriy.afonin@walletone.com>
2018-05-11 14:39:44 +0500

    Common module was added #18

---
+++ src/Simpbot.Core/Modules/Common.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+using Discord.Commands;
+
+namespace Simpbot.Core.Modules
+{
+    public class Common : ModuleBase
+    {
+        [Command("greenfrog", RunMode = RunMode.Async)]
+        public Task GreenFrog()
+        {
+            return ReplyAsync(@"https://www.youtube.com/watch?v=JybITdCrqyU");
+        }
+    }
+}
