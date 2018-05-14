a67c5300a84bb238152c99ab4423f3b26d0b28d5
Afonin Dmitry <dmitriy.afonin@walletone.com>
2018-05-14 12:52:50 +0500

    Cleanups #38

--- src/Simpbot.Core/Modules/Bot.cs
+++ src/Simpbot.Core/Modules/Bot.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +8,7 @@ using Discord;
 using Discord.Commands;
 
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 
 using Simpbot.Core.Persistence;
 using Simpbot.Core.Persistence.Entity;
@@ -20,11 +20,13 @@ namespace Simpbot.Core.Modules
     {
         private readonly CommandService _commandService;
         private readonly StorageContext _prefixContext;
+        private readonly MemoryCache _memoryCache;
 
-        public Bot(CommandService commandService, StorageContext prefixContext)
+        public Bot(CommandService commandService, StorageContext prefixContext, IMemoryCache memoryCache)
         {
             _commandService = commandService;
             _prefixContext = prefixContext;
+            _memoryCache = memoryCache as MemoryCache;
         }
 
         [Command("info", RunMode = RunMode.Async), Summary("info about the bot")]
@@ -72,10 +74,13 @@ namespace Simpbot.Core.Modules
         {
             var heapMemory = GC.GetTotalMemory(true) * 0.000001 + " mb in use";
             var allMemory = Process.GetCurrentProcess().PrivateMemorySize64 * 0.000001 + " mb in use";
-            
+            var memoryCash = $"Elements in the cache {_memoryCache.Count}";
+
+
             var embed = new EmbedBuilder()
                 .WithTitle("**Memory usage**")
                 .AddField("**GC Heap memory**", heapMemory, true)
+                .AddField("**Cached**", memoryCash, true)
                 .AddField("**All memory**", allMemory, true)
                 .WithColor(Color.DarkGreen)
                 .Build();
